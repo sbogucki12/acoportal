e27107e358d625a3d76d1269911e9d4dda0b62cb
Steve Bogucki <sbogucki@mail.usf.edu>
2018-08-19 17:26:57 -0700

    adding authentication to server

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('./models/WtsRecord');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('flash');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 if(process.env.NODE_ENV === 'production'){
     mongoose.connect(process.env.mongoURI)
@@ -12,6 +24,7 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 const WtsRecord = mongoose.model('wtsrecords');
+const User = require('./models/User');
 
 app.get('/api/wtsrecords', cors(), (req, res) => {
     WtsRecord.find({})
@@ -30,12 +43,99 @@ app.get('/api/wtsrecord', cors(), (req, res) => {
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
-    const path = require('path');
-
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))   
     })
 }
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true, 
+    resave: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressValidator({
+    errorFormatter: (param, msg, value) => {
+        const namespace = param.split('.')
+        , root          = namespace.shift()
+        , formParam     = root; 
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param   :   formParam, 
+            msg     :    msg, 
+            value   :   value
+        };
+    }
+}));
+
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.post('/api/register', cors(), (req, res) => {
+    const firstName     = req.body.firstName;
+    const lastName      = req.body.lastName; 
+    const company       = req.body.company;
+    const email         = req.body.email; 
+    const password      = req.body.password; 
+    const password2     = req.body.password2;
+    const street        = req.body.street; 
+    const city          = req.body.city; 
+    const usState       = req.body.usState; 
+    const zipcode       = req.body.zipcode;
+    const phone         = req.body.phone;  
+    const role          = req.body.role; 
+    const advisor       = req.body.advisor;
+    
+    req.checkBody('firstName', 'First name is required').notEmpty();
+    req.checkBody('lastName', 'Last name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('role', 'Association to LAACO is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+    const errors = req.validationErrors();
+
+    if(errors){
+        res.send(errors)
+    } else {
+        const newUser = new User({
+            firstName: firstName, 
+            lastName: lastName, 
+            company: company, 
+            email: email, 
+            password: password,             
+            street: street, 
+            city: city, 
+            usState: usState, 
+            zipcode: zipcode, 
+            phone: phone, 
+            role: role, 
+            advisor: advisor
+        })
+
+        User.createUser(newUser, (err, user) => {
+            if(err) throw err; 
+            console.log(user);
+        })
+
+        // req.flash('success_msg', 'You are registered and can now log in.')
+        res.send("You are logged in.")
+    }
+})
+
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT);
---
+++ models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    company: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    password2: {
+        type: String
+    },
+    street: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    state: {
+        type: String
+    },
+    zipcode: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    role: {
+        type: String
+    },
+    advisor: {
+        type: String
+    }   
+})
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.createUser = (newUser, callback) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            newUser.password = hash;
+            newUser.save(callback)
+        })
+    })
+}
